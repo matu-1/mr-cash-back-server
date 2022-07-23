9d468fa0d38a7abb790011f3685376de23bd65d8
Matias Manuel Flores <arnod7991@gmail.com>
2022-07-23 14:06:32 -0400

    email template

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { SendGridService } from '@anchan828/nest-sendgrid';
 import { preaprovedTemplate } from './utils/preapproved_email';
 import { EmailDto } from './dtos/email.dto';
+import { CREDIT_STATUS } from '../credit/credit.constant';
+import { newCreditTemplate } from './utils/new_credit_email';
 
 @Injectable()
 export class EmailService {
@@ -10,12 +12,25 @@ export class EmailService {
     }
 
     async sendEmail(dto: EmailDto) {
+        let subject = "Mr Cash Back";
+        let html = `<b>Mr Cash Back</b>`;
+        switch (dto.status) {
+            case CREDIT_STATUS.PENDING:
+                subject = "Nuevo credito Mr Cash Back";
+                html = newCreditTemplate(dto);
+                break;
+            case CREDIT_STATUS.PREAPPROVED:
+                subject = "Tu credito ha sido pre aprobado";
+                html = preaprovedTemplate(dto);
+                break;
+        }
         await this.sendGrid.send({
             to: dto.email,
             from: process.env.FROM_EMAIL,
-            subject: "Tu credito ha sido pre aprobado",
+            bcc: ["jpsalinas@motoclickapp.com", "angela@patio.com.bo"],
+            subject: subject,
             text: dto.creditId,
-            html: preaprovedTemplate(dto),
+            html: html,
         });
     }
 }
